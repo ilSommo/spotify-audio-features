ad19711ace4b2a342630cdbb9b282a886b82c6b1
ilSommo <martinomarelakota@yahoo.it>
2021-04-18 21:13:22 +0200

    Add difference graph and formatting
    * functions.py: add difference graph function and formatting
    * spotify-audio-features.ipynb: implement difference graph and formatting

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-#TODO formatting
 import matplotlib.pyplot as plt
 import numpy as np
 from pgmpy.factors.discrete import State, TabularCPD
 
+
 def probability_text(variable, evidence):
+
 	if not evidence:
 		text = "P(" + variable + ")"
 	else:
@@ -11,7 +12,9 @@ def probability_text(variable, evidence):
 		for e in evidence.items():
 			text += " " + str(e[0]) + "=" + str(e[1])
 		text += ")"
+
 	return(text)
+
 
 def exact(exact_inference, variable, evidence = {}):
 	
@@ -26,6 +29,7 @@ def exact(exact_inference, variable, evidence = {}):
 
 	print(text)
 	print(cpd)
+
 
 def likelihood_weighted(approximate_inference, variable, evidence = {}, size = 10000):
 	
@@ -47,6 +51,7 @@ def likelihood_weighted(approximate_inference, variable, evidence = {}, size = 1
 	print(text)
 	print(cpd)
 
+
 def rejection(approximate_inference, variable, evidence = {}, size = 10000):
 	
 	text = probability_text(variable, evidence)
@@ -67,15 +72,18 @@ def rejection(approximate_inference, variable, evidence = {}, size = 10000):
 	print(text)
 	print(cpd)
 
+
 def graph_points(exact_inference, approximate_inference, variable, evidence, starting_size = 10, final_size = 100000, experiments = 20):
-	evidence_state = []
-	for e in evidence.items():
-		evidence_state.append(State(e[0], e[1]))
+
+	sizes = list(np.logspace(np.log10(starting_size), np.log10(final_size), num = experiments, dtype = '<i8'))
 	exact_query = exact_inference.query([variable], evidence, show_progress = False)
 	likelihood_weighted_results = {}
 	rejection_results = {}
-	sizes = list(np.logspace(np.log10(starting_size), np.log10(final_size), num = experiments, dtype = '<i8'))
+
 	state_names = exact_query.state_names[variable]
+	evidence_state = []
+	for e in evidence.items():
+		evidence_state.append(State(e[0], e[1]))
 	for state in state_names:
 		likelihood_weighted_results[state] = []
 		rejection_results[state] = []
@@ -87,21 +95,47 @@ def graph_points(exact_inference, approximate_inference, variable, evidence, sta
 			rejection_query = approximate_inference.rejection_sample(evidence = evidence_state, size = size, show_progress = False)
 			rejection_probability = np.count_nonzero(rejection_query[variable] == state) / size
 			rejection_results[state].append(rejection_probability)
+
 	return sizes, dict(zip(state_names, exact_query.values)), likelihood_weighted_results, rejection_results
 
+
 def graph(sizes, exact_results, likelihood_weighted_results, rejection_results, variable, evidence):
+
 	text = probability_text(variable, evidence)
+	text += " – Probabilities comparison"
 	print(text)
 
-	plt.figure(figsize=(20,5))
-	i = 1
+	plt.figure(figsize=(20, 5))
+	i = 0
 	for state in exact_results.keys():
-		plt.subplot(1, len(exact_results.keys()), i)
 		i += 1
-		plt.ylim(0,1)
+		plt.subplot(1, len(exact_results.keys()), i)
+		plt.xlim(sizes[0], sizes[-1])
+		plt.ylim(0, 1)
 		plt.title(state)
-		likelihood_weighted_plot, = plt.semilogx(sizes, likelihood_weighted_results[state],label="Likelihood weighted sampling")
-		rejection_plot, = plt.semilogx(sizes, rejection_results[state],label="Rejection sampling")
-		exact_plot, = plt.semilogx(sizes, exact_results[state]*np.ones(len(sizes)),label="Exact inference")
-		plt.legend(handles=[exact_plot,likelihood_weighted_plot,rejection_plot])
+		likelihood_weighted_plot, _ = plt.semilogx(sizes, likelihood_weighted_results[state], label = "Likelihood weighted sampling")
+		rejection_plot, _ = plt.semilogx(sizes, rejection_results[state], label = "Rejection sampling")
+		exact_plot, _ = plt.semilogx(sizes, exact_results[state] * np.ones(len(sizes)), label = "Exact inference")
+		plt.legend(handles = [exact_plot, likelihood_weighted_plot, rejection_plot])
+	plt.show()
+
+
+def graph_diff(sizes, exact_results, likelihood_weighted_results, rejection_results, variable, evidence):
+	
+	text = probability_text(variable, evidence)
+	text += " – Differences comparison"
+	print(text)
+
+	plt.figure(figsize=(20, 5))
+	i = 0
+	for state in exact_results.keys():
+		i += 1
+		plt.subplot(1, len(exact_results.keys()), i)
+		plt.xlim(sizes[0], sizes[-1])
+		plt.ylim(-0.5, 0.5)
+		plt.title(state)
+		likelihood_weighted_plot, _ = plt.semilogx(sizes, likelihood_weighted_results[state] - exact_results[state], label = "Likelihood weighted sampling")
+		rejection_plot, _ = plt.semilogx(sizes, rejection_results[state] - exact_results[state], label = "Rejection sampling")
+		exact_plot, _ = plt.semilogx(sizes, np.zeros(len(sizes)), label = "Exact inference")
+		plt.legend(handles = [exact_plot, likelihood_weighted_plot, rejection_plot])
 	plt.show()
