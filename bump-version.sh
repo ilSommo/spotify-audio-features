1662467ca0b6a19b8931a97b566cc632684fb842
ilSommo <martinomarelakota@yahoo.it>
2021-04-20 10:40:45 +0200

    Bump version number to 1.0.0-alpha.3

--- bump-version.sh
+++ bump-version.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sed -i '' 's/__version__ = .*/__version__ = '$1'/' functions.py
-sed -i '' 's/__version__ = .*/__version__ = '$1'/' spotify-audio-features.ipynb
+sed -i '' 's/__version__ = .*/__version__ = '\'$1\''/' functions.py
+sed -i '' 's/__version__ = .*/__version__ = '\'$1\''\\n",/' spotify-audio-features.ipynb
 
 git commit -a -m "Bump version number to "$1""
+git checkout master
+git merge release-$1
+git tag $1
+git checkout develop
+git merge release-$1
+git branch -d release-$1
